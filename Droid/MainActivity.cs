2366312d36e115c2534b9029e061b8872dc94df4
JonathanWinnaar <JonathanWinnaar@JONATHANWIN80D5>
2016-11-18 16:38:00 +0200

    Add project files.

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Gcm.Client;
+
+namespace praat.Droid
+{
+	[Activity (Label = "praat.Droid",
+		Icon = "@drawable/icon",
+		MainLauncher = true,
+		ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation,
+		Theme = "@android:style/Theme.Holo.Light")]
+	public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Initialize Azure Mobile Apps
+			Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
+
+			// Initialize Xamarin Forms
+			global::Xamarin.Forms.Forms.Init (this, bundle);
+
+			// Load the main application
+			LoadApplication (new App ());
+
+
+		    instance = this;
+
+            try
+            {
+                // Check to ensure everything's setup right
+                GcmClient.CheckDevice(this);
+                GcmClient.CheckManifest(this);
+
+                // Register for push notifications
+                System.Diagnostics.Debug.WriteLine("Registering...");
+                GcmClient.Register(this, PushHandlerBroadcastReceiver.SENDER_IDS);
+            }
+            catch (Java.Net.MalformedURLException)
+            {
+                CreateAndShowDialog("There was an error creating the client. Verify the URL.", "Error");
+            }
+            catch (Exception e)
+            {
+                CreateAndShowDialog(e.Message, "Error");
+            }
+
+        }
+
+        private void CreateAndShowDialog(String message, String title)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.SetMessage(message);
+            builder.SetTitle(title);
+            builder.Create().Show();
+        }
+
+        // Create a new instance field for this activity.
+        static MainActivity instance = null;
+
+        // Return the current activity instance.
+        public static MainActivity CurrentActivity
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+
+    }
+}
+
---
+++ praat/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace praat
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://praat.azurewebsites.net";
+	}
+}
+
